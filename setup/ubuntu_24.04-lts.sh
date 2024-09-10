0693141bffd9406e2670fc77c207513340939890
Mohammad Kibria <106745675+kibria5@users.noreply.github.com>
2024-09-10 23:59:30 +0600

    Update ubuntu_24.04-lts.sh

--- setup/ubuntu_24.04-lts.sh
+++ setup/ubuntu_24.04-lts.sh
@@ -34,7 +34,7 @@ sudo apt update
 sudo apt install -y gh
 
 echo -e "Setting up udev rules for adb!"
-sudo curl --create-dirs -L -o /etc/udev/rules.d/51-android.rules -O -L https://raw.githubusercontent.com/M0Rf30/android-udev-rules/master/51-android.rules
+sudo curl --create-dirs -L -o /etc/udev/rules.d/51-android.rules -O -L https://raw.githubusercontent.com/M0Rf30/android-udev-rules/main/51-android.rules
 sudo chmod 644 /etc/udev/rules.d/51-android.rules
 sudo chown root /etc/udev/rules.d/51-android.rules
 sudo systemctl restart udev
